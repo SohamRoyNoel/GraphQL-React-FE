ab99f0f9a92df185bcb7d63a75a62d7648dde5ab
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-03 17:57:09 +0530

    Mutation with FE

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,8 +1,18 @@
 import React, { Component } from "React";
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+import query from "../queries/fetchSongs";
+import mutation from "../queries/createSongs";
 
 class SongCreate extends Component {
+
+      sleep(ms) {
+            return new Promise(
+                  resolve => setTimeout(resolve, ms)
+            );
+      }
+
       constructor(props) {
             super(props);
             this.state = { title: ''};
@@ -10,14 +20,24 @@ class SongCreate extends Component {
 
       onSubmit(event) {
             event.preventDefault();
-
+            // Embed the variable to mutation GQL query
+            this.props.mutate ({
+                  variables: {title: this.state.title},
+                  refetchQueries: [{ query: query, /*variables: <Vars that can be passed> */ }]
+            }).then(() => {
+                  this.sleep(1000);
+                  hashHistory.push('/')
+            });
       }
 
       render() {
             return (
-                  <div>
-                        <h3>Create A New Song</h3>
-                       <form onSubmit={this.onSubmit.bind(this)}>
+                  <div className="container">
+                        <Link to="/">Back</Link>
+                        <br />                        
+                        <h3>Create A New Song</h3>                    
+                        
+                        <form onSubmit={this.onSubmit.bind(this)}>
                               <label>Song Title</label>
                               <input 
                                     onChange={event => this.setState({title: event.target.value})}
@@ -28,14 +48,13 @@ class SongCreate extends Component {
             );
       }
 }
-// 
+
 // const mutation = gql`
-      // mutation {
-      // addSong(title: "Ass Cooker"){
-            // id
+      // mutation AddSong($title: String) {
+      // addSong(title: $title){
             // title
       // }
 // }
 // `;
 
-export default SongCreate;
+export default graphql(mutation) (SongCreate);
